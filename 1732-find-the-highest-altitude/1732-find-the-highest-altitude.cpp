c3fd6896375f5cd70fa51b3643f60e1c018a4bfa
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-03-06 16:19:38 +0600

    Time: 1 ms (4.06%), Space: 10.7 MB (74.58%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+
+        int n = gain.size();
+
+        int high = 0;
+        int sum = 0;
+
+        for(int i = 0; i<n; i++)
+        {
+            sum += gain[i];
+            high = max(sum, high);
+        }
+
+        return high;
+        
+    }
+};
