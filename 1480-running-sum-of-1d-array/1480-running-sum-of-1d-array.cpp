ca8f39525a282dbc519a7868b2bf31245717a71d
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-03-06 16:06:11 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cpp
@@ -3,16 +3,13 @@ public:
     vector<int> runningSum(vector<int>& nums) {
 
         int n = nums.size();
-        vector<int> prefix(n);
-
-        prefix[0] = nums[0];
 
         for(int i = 1; i<n; i++)
         {
-           prefix [i] = prefix[i-1] + nums[i];
+           nums[i] +=nums[i-1];
         }
 
-        return prefix;
+        return nums;
         
         
     }
