b16928f16f324ae819a0c3ce99a465b73b93e34c
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-03-09 23:34:06 +0600

    Time: 0 ms (100%), Space: 17.7 MB (8.58%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+
+     int x = 0;
+        for(string op : operations)
+        {
+            if(op[1] == '+')
+            {
+                x += 1;
+            }else x-=1;
+        }return x;
+        
+    }
+};
