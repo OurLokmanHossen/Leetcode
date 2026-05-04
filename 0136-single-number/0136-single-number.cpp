474e59c807a9a8decf09975c7cbb7e81505fddc8
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-05-04 22:28:19 +0600

    Time: 0 ms (100%), Space: 20.7 MB (57.71%) - LeetHub

---
+++ 0136-single-number/0136-single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int n = nums.size();
+
+        int ans = 0;
+
+        for(auto it : nums)
+        {
+            ans ^= it;
+        }
+
+        return ans;
+
+        
+        
+    }
+};
