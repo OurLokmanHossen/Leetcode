d76f4aac1feb4faa6d97eaa5a04bd7f9b581825e
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-08-23 11:25:54 +0600

    Time: 7 ms (18.12%), Space: 9.7 MB (49.55%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        int n = s.size();
+        int m = t.size();
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        if(n != m) return false;
+
+        bool f = true;
+
+        for(int i = 0; i<n; i++)
+        {
+            if(s[i] != t[i]) {f = false; break;}
+        }
+
+        if(f) return true;
+        else return false;
+        
+    }
+};
