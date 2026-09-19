111c3992517bb1e8931fd0dea995a6ff42ea7b2f
Md. Lokman Hossen <mhlokman10@gmail.com>
2026-09-19 11:32:45 +0600

    Time: 4 ms (34.44%), Space: 18.1 MB (33.88%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        map<int,int> mp;
+        for(auto it : nums)
+        {
+            mp[it]++;
+        }
+        priority_queue<pair<int,int>> pq;
+        for(auto x: mp)
+        {
+            pq.push({x.second, x.first});
+        }
+        
+        vector<int> ans;
+        while(k--)
+        {
+            auto y = pq.top();
+            ans.push_back(y.second);
+            pq.pop();
+        }
+        return ans;
+        
+    }
+};
